a0c7a0f3c45d7e01057c6650c9c7534a4b8084c9
Nikita Zverev <zverev@creograf.ru>
2017-04-06 16:44:39 +0500

    Server return index.html from client/dist

--- server/views.py
+++ server/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+
 
 # Create your views here.
+def get_index(request):
+    if request.method == "GET":
+        args = {"user": request.user}
+        return render_to_response('index.html', args)
